e6e181b25ea30d29351c84eda70636e8b0050568
Lucas Cherkewski <lucas@lucascherkewski.com>
2022-10-19 21:03:53 -0400

    parse salary orders post-2015

--- analysis/deputy-salary-orders.R
+++ analysis/deputy-salary-orders.R
@@ -21,7 +21,7 @@ deputy_salary_order_attachments %>%
   select(id, pc_number, date, text) %>%
   write_csv("data/out/deputy-salary-order-attachments.csv")
 
-salary_revisions <- deputy_salary_order_attachments %>%
+salary_revisions_raw <- deputy_salary_order_attachments %>%
   mutate(
     text = str_remove_all(text, regex("^PC Number: [0-9]{4}-[0-9]{4}$", multiline = TRUE)), # opening PC number
     text = str_remove_all(text, regex("^Date: [0-9]{4}-[0-9]{2}-[0-9]{2}$", multiline = TRUE)), # opening date
@@ -29,7 +29,11 @@ salary_revisions <- deputy_salary_order_attachments %>%
     text = str_remove_all(text, regex("^…/[0-9]+$|^ ?- [0-9]+ -$", multiline = TRUE)), # page numbers
     text = str_replace_all(text, coll("; and"), ";"),
     text = str_replace_all(text, coll(";"), "."),
-    text = str_replace_all(text, regex("([a-z,])(\n\n)"), "\\1 ") # fix for 2+ line breaks within an entry
+    text = str_replace_all(text, regex("([a-z,])(\n\n)"), "\\1 "), # fix for 2+ line breaks within an entry
+    text = str_replace_all(text, coll("–"), "-"),
+    text = str_replace_all(text, coll("witihin"), "within"),
+    text = str_replace_all(text, coll("andwithin"), "and within"),
+    text = str_replace_all(text, coll("within the ange"), "within the range")
   ) %>%
   unnest_tokens(salary_revision, text, token = "paragraphs", to_lower = FALSE) %>%
   mutate(
@@ -37,15 +41,15 @@ salary_revisions <- deputy_salary_order_attachments %>%
   ) %>%
   filter(
     ! salary_revision == "",
-    ! str_detect(salary_revision, "échelle|compter du"), # French entries
+    ! str_detect(salary_revision, "échelle|compter du|à compter"), # French entries
     ! str_detect(salary_revision, "^Sur recommandation du premier ministre"), # French entries
     ! str_detect(salary_revision, "^…"), # errant page number
     ! str_detect(salary_revision, "in the amount and on the date indicated in the annexed schedule, of:$") # intro phrases for post-2015 revisions
   )
 
 # for debugging / text reference
-salary_revisions %>%
-  write_csv("data/out/deputy-salary-revisions.csv")
+salary_revisions_raw %>%
+  write_csv("data/out/deputy-salary-revisions-raw.csv")
 
 
 
@@ -68,15 +72,50 @@ salary_revisions %>%
 #   - salary max
 #   - effective date (occasionally with specific "from" and "to")
 
-testing_post_2015 <- salary_revisions %>%
+salary_revisions_post_2015 <- salary_revisions_raw %>%
   filter(year(date) > 2015) %>%
-  slice_sample(n = 10)
-
-salary_revisions %>%
-  filter(year(date) > 2015) %>%
-  # edge case: "Natynczyk, Walter J., General (Retired), ..."
-  # edge case: "Sabia, Michael Jonathan, O.C., ..."
-  # edge case: "Shugart, P.C., Senator the Honourable Ian"
+  mutate(
+    # Handle some name edge cases:
+    # - "Natynczyk, Walter J., General (Retired), ..."
+    # - "Sabia, Michael Jonathan, O.C., ..."
+    # - "Shugart, P.C., Senator the Honourable Ian"
+    salary_revision = str_remove_all(
+      salary_revision,
+      paste("General \\(Retired\\), ", "O\\.C\\., ", "P\\.C\\., Senator the Honourable", sep = "|")
+    ),
+    salary_revision = str_replace_all(
+      salary_revision,
+      "within the range of",
+      "within the range"
+    )
+  ) %>%
   separate(salary_revision, sep = ", ", into = c("name_last", "name_first", "salary_revision"), extra = "merge") %>%
   separate(salary_revision, sep = ", within the range \\(", into = c("position", "salary_revision"), extra = "merge") %>%
-  separate(salary_revision, sep = " – | - |\\), ", into = c("salary_min", "salary_max", "salary_revision"), extra = "merge")
+  mutate(
+    salary_revision = paste0(", within the range (", salary_revision),
+    salary_revision = str_replace_all(salary_revision, " and within the range", ", within the range"),
+    salary_revision = str_replace_all(salary_revision, ",within the range", ", within the range"),
+    salary_revision = str_remove(salary_revision, ".$")
+  ) %>%
+  mutate(
+    salary_revision = str_split(salary_revision, ", within the range \\(")
+  ) %>%
+  unnest_longer(salary_revision) %>%
+  filter(! salary_revision == "") %>%
+  mutate(
+    salary_revision = str_replace_all(salary_revision, coll(") effective"), "), effective"),
+    salary_revision = str_replace_all(salary_revision, coll(") and effective"), "), effective")
+  ) %>%
+  separate(salary_revision, sep = " – | - |\\), ", into = c("salary_min", "salary_max", "salary_revision"), extra = "merge") %>%
+  extract(
+    salary_revision,
+    into = c("start", "end"),
+    "(?:commencing|effective)(?: on)? ([A-Za-z]* ?[0-9]+ ?, ?[0-9]{4})(?: and ending)?(?: on)? ?([A-Za-z]* ?[0-9]+ ?, ?[0-9]{4})?"
+  ) %>%
+  mutate(
+    position = str_remove_all(position, "^former "),
+    end = if_else(end == "", NA_character_, end),
+    start = mdy(start),
+    end = mdy(end),
+    across(contains("salary"), ~ as.integer(str_remove_all(.x, "[^0-9]")))
+  )
